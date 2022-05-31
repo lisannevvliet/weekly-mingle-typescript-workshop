0e61b8d68fbb8d9052c67622cba6ef561126422f
Gavin Ligthart <gavin.ligthart@labela.nl>
2022-05-31 17:37:50 +0200

    Add extra documentation to the third exercise

--- src/exercises/3/index.ts
+++ src/exercises/3/index.ts
@@ -3,7 +3,9 @@
  * Exercise 3
  * Creating and reusing types
  *
- * https://www.typescriptlang.org/docs/handbook/2/objects.html
+ * https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html#defining-types
+ * At label A, we prefer 'type' over 'interface'. They are very similar, but types give us a bit more reusability.
+ * They are however very similar, so you can look over the fact that they are not the same.
  *
  * TO RUN TEST
  * npm run test-3
@@ -15,13 +17,12 @@
  * ======================================================
  * TODO:
  * 1. Type the 'Employee' object
- * 2. Add the type to the 'employee' variable
  * ======================================================*/
 
 export type Employee = {};
 
 export function exercise0301() {
-  const employee = {
+  const employee: Employee = {
     name: 'Timothy',
     age: 25,
     city: 'Amsterdam',
@@ -48,13 +49,12 @@ export function exercise0301() {
  * TODO:
  * 1. Type the 'Company' object
  * 2. Reuse the 'Person' type within the new 'Company' type
- * 3. Add type to the 'company' const
  * ======================================================*/
 
 export type Company = {};
 
 export function exercise0302() {
-  const company = {
+  const company: Company = {
     name: 'Label A',
     address: 'Max Euweplein 24',
     postal_code: '1017MB',
@@ -80,7 +80,7 @@ export function exercise0302() {
   /* TIP
    * ======================================================
    * Sometimes you're not sure if variable in an object is filled in, for example while using an API.
-   * To combat this you can prefix the type with a '?', like so,
+   * To combat this you can prefix the type with a '?', like so;
    * {
    *   name?: string;
    * }
